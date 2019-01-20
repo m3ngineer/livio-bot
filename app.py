d91cec8851a41a6019297ee0c0a417b3bc9576a6
Matthew Eng <mattheweng@192.168.1.40>
2019-01-20 12:20:04 -0500

    Add gitignore file

--- app.py
+++ app.py
@@ -11,6 +11,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+
+        return response.query_result.fulfillment_text
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+    response_text = { "message":  fulfillment_text }
+
+    return jsonify(response_text)
+
 # run Flask app
 if __name__ == "__main__":
     app.run()
